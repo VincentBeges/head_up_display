84453957a05c339d5c5fad2ed3add6192792f200
Vincent <vincent.beges@gmail.com>
2024-09-23 06:08:15 +0200

    docs: improved attribute override with env var doc

--- head_up_display/hud/generation_config.py
+++ head_up_display/hud/generation_config.py
@@ -11,6 +11,16 @@ class GenerationConfig(BaseSettings):
 
     What is writen in the overlay is defined using templates elements.
     The way we process media before writing hud is defined in this class.
+
+    Any of the attributes bellow can be override using env variable (except model_config).
+    >>> import os
+    >>> os.environ['hud_generation_config__do_resize'] = 'False'
+    >>> config = GenerationConfig()
+    >>> print(config.do_resize)
+    >>> # False
+    The environment variable name is composed of env_prefix (defined in current model_config) + attribute name
+
+    https://docs.pydantic.dev/latest/concepts/pydantic_settings/#usage
     """
     model_config = SettingsConfigDict(env_prefix=constants.GENERATION_CONFIG_ENV_PREFIX)
 
